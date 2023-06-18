57aafc6399cba88feb3a04ef3db98b57cb6daa11
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-18 17:23:48 +0300

    Added pages

---
+++ screens/RegistrationScreen.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
+
+export const RegistrationScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../img/photo-bg.png")}
+        resizeMode="cover"
+        style={styles.image}
+      />
+      <View style={styles.form}>
+        <Image />
+        <Text>Реєстрація</Text>
+        <View>
+          <TextInput placeholder="Логін"></TextInput>
+          <TextInput placeholder="Адреса електронної пошти"></TextInput>
+          <TextInput placeholder="Пароль"></TextInput>
+        </View>
+        <Button title="Зареєстуватися" />
+        <Text>Вже є акаунт? Увійти</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+  },
+  form: {},
+});
