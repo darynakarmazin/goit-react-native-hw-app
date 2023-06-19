af38f6d921f9c514feea9053a792601be5ab366a
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-19 16:54:28 +0300

    Added google-fonts to project

--- App.js
+++ App.js
@@ -3,8 +3,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { RegistrationScreen } from "./screens/RegistrationScreen";
 import { LoginScreen } from "./screens/LoginScreen";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
   return (
     <View style={styles.container}>
       <RegistrationScreen />
