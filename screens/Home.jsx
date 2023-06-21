531c7b87a68ac723ef09c8088cba95a6f8c85bb4
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-21 16:23:01 +0300

    Add bottom navigation to Home page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import PostsScreen from "./PostsScreen";
+import CreatePostsScreen from "./CreatePostsScreen";
+import ProfileScreen from "./ProfileScreen";
 
+const Tabs = createBottomTabNavigator();
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <Tabs.Navigator
+      initialRouteName="PostsScreen"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Profile") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          }
+          // return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tabs.Screen
+        name="PostsScreen"
+        component={PostsScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="CreatePostsScreen"
+        component={CreatePostsScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tabs.Navigator>
   );
 };
 
--- screens/PostsScreen.jsx
+++ screens/PostsScreen.jsx
@@ -1,7 +1,20 @@
-import { Text } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const PostScreen = () => {
-  return <Text>PostScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default PostScreen;
